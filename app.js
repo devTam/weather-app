880aae5d711ab1748df66c401f3f518f4cc31215
Teebreezy101 <tammy.batubo@gmail.com>
2020-09-04 05:11:50 +0100

    add js logic

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 const form = document.getElementById('form');
-const loc = document.getElementById('location');
-const temp = document.getElementById('temperature');
-const humid = document.getElementById('humidity');
-const condition = document.getElementById('condition');
-const error = document.getElementById('error');
-const API_KEY = '10fdc6d6b455457b79f01f3317b2c222';
-// const API_CALL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+const locBox = document.getElementById('location');
+const tempBox = document.getElementById('temperature');
+const humidBox = document.getElementById('humidity');
+const conditionBox = document.getElementById('condition');
+const errorBox = document.getElementById('error');
 
-const getWeather = (e) => {
+let city = document.getElementById('city');
+
+const populate = (data) => {
+    locBox.innerHTML = data.name;
+    tempBox.innerHTML = `${data.main.temp}&#8451;`;
+    humidBox.innerHTML = data.main.humidity;
+    conditionBox.innerHTML = data.weather[0].description;
+    errorBox.style.display = 'none';
+}
+
+const depopulate = () => {
+    locBox.innerHTML = '';
+    tempBox.innerHTML = '';
+    humidBox.innerHTML = '';
+    conditionBox.innerHTML = '';
+    errorBox.style.display = "block";
+}
+
+
+const getWeather = async (e) => {
     e.preventDefault();
+    const API_KEY = '10fdc6d6b455457b79f01f3317b2c222';
     
+    
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${API_KEY}`);
+        const data = await response.json();
+
+        if(data.cod != "404") {
+            populate(data);
+            
+        } else {
+            depopulate();
+        };
+    } catch (error) {
+        console.log(error);
+        depopulate();
+        
+    } finally {
+        city.value = "";
+    }
+
+   
+
 }
 
 
